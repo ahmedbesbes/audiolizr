93e16c5cf2575e5646b053355f331146aa00cc79
Ahmed Besbes <ahmed.besbes@hotmail.com>
2022-12-11 11:54:50 +0100

    Raise error when video exceeds 6 minutes

--- src/runners/video_downloader.py
+++ src/runners/video_downloader.py
@@ -1,4 +1,5 @@
 import bentoml
+from bentoml.exceptions import BentoMLException
 from pytube import YouTube
 
 
@@ -9,6 +10,12 @@ class VideoDownloader(bentoml.Runnable):
     @bentoml.Runnable.method(batchable=False)
     def download_video(self, url):
         youtube = YouTube(url)
+
+        if youtube.length > 360:
+            raise BentoMLException(
+                "The YouTube video you submitted is too long (exceeds 6 mins)",
+            )
+
         path = youtube.streams.filter(only_audio=True)[0].download(
             filename="/tmp/output.mp4",
         )
